d26a567ca360f30545502d44a0dbc0cda6dfec67
Eric <tryharder3l33t@gmail.com>
2022-03-16 15:21:06 -0400

    react loaded

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const seed = require("./bin/seed");
+const path = require("path");
 app.listen(process.env.PORT || port, () => {
   console.log(`Listening on port ${port}`);
 });
 
+app.use("/dist", express.static(path.join(__dirname, "dist")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 seed();
 
 module.exports = app;
